5472cb42ef85df3877e6bf0bff92d003b7cbba52
Your Name <you@example.com>
2024-06-05 23:10:39 +0200

    lesson_34/code/homework_34/Task.java

---
+++ lesson_34/code/homework_34/Task.java
@@ -0,0 +1,62 @@
+package homework_34;
+
+import java.util.Objects;
+
+public class Task implements  Comparable <Task>{
+    private int id;
+    private String task;
+    int taskNumber;
+
+    public Task(int i, String note, int i1) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getTaskNumber() {
+        return taskNumber;
+    }
+
+    public void setTaskNumber(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", task='" + task + '\'' +
+                ", taskNumber=" + taskNumber +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task task)) return false;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(Task o) {
+        return this.getTaskNumber() - o.getTaskNumber(); // comparison of objects by field taskNumber
+    }
+}
