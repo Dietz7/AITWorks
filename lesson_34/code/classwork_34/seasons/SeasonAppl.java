115af1523746930eb1bebb4c1b70b336e7ad0589
Your Name <you@example.com>
2024-06-08 16:07:56 +0200

    lesson_34/code/classwork_34/seasons/SeasonAppl.java

--- lesson_34/code/classwork_34/seasons/SeasonAppl.java
+++ lesson_34/code/classwork_34/seasons/SeasonAppl.java
@@ -13,18 +13,18 @@ public class SeasonAppl {
 
         Season[] seasons = Season.values(); // fill array
         for (int i = 0; i < seasons.length; i++) {
-            // System.out.println(seasons[i]);
+            //System.out.println(seasons[i]);
             //System.out.println(seasons[i].getName());
             System.out.println(seasons[i].getNumber() + "." + seasons[i].getName());
         }
         System.out.println("---------------------------------------");
-            //foreach
-         for (Season s : seasons) {
-             System.out.println(s.getNumber()+ ". " + s.getName());
-         }
+        //foreach
+        for (Season s : seasons) {
+            System.out.println(s.getNumber() + ". " + s.getName());
+        }
 
         System.out.println("---------------toString-------------------");
-         String str = Season.AUTUMN.toString();
+        String str = Season.AUTUMN.toString();
         System.out.println(str);
 
         System.out.println("--------------ordinal()-------------------");
