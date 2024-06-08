874d04dd951998bd5a8d704b26b8ca5adf6beb8b
Your Name <you@example.com>
2024-06-08 16:06:53 +0200

    lesson_34/code/homework_34/tests/

---
+++ lesson_34/code/homework_34/tests/ToDoListImplTest.java
@@ -0,0 +1,52 @@
+package homework_34.tests;
+
+import homework_34.model.Task;
+import homework_34.dao.ToDoListImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoListImplTest {
+    private ToDoListImpl toDoList;
+
+    @BeforeEach
+    void setUp() {
+        toDoList = new ToDoListImpl(5);
+    }
+    @Test
+    void addNote() {
+        Task task1 = new Task(0, "Finishing homework");
+        toDoList.addNote(task1);
+        List<Task> tasks = toDoList.seeAllNotes();
+        System.out.println(toDoList.seeAllNotes());
+        assertEquals(1, tasks.size());
+        assertEquals("Finishing homework", tasks.get(0).getTask());
+    }
+
+    @Test
+    void deleteNote() {
+        toDoList.addNote("Finishing homework");
+        toDoList.addNote("Meeting with Mama");
+        toDoList.addNote("Spending time with husband");
+        toDoList.deleteNote(1);
+        List<Task> tasks = toDoList.seeAllNotes();
+        assertEquals(2, tasks.size());
+        assertEquals("Meeting with Mama", tasks.get(0).getTask());
+        assertEquals("Spending time with husband", tasks.get(1).getTask());
+        System.out.println(tasks);
+    }
+
+    @Test
+    void editNote() {
+        toDoList.addNote("Meeting with Mama");
+        System.out.println(toDoList.seeAllNotes());
+        toDoList.editNote(1, "Drinking coffee with Mama");
+        List<Task> tasks = toDoList.seeAllNotes();
+        assertEquals(1, tasks.size());
+        assertEquals("Drinking coffee with Mama", tasks.get(0).getTask());
+        System.out.println(toDoList.seeAllNotes());
+    }
+}
