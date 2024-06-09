94cb8264e1bdbc8fac61add0f6024abdc8dc51b4
Your Name <you@example.com>
2024-06-09 07:31:24 +0200

    lesson_32/code/homework_32/model/Smartphone.java

---
+++ lesson_32/code/homework_32/model/Smartphone.java
@@ -0,0 +1,23 @@
+package homework_32.model;
+
+import java.math.BigInteger;
+
+public class Smartphone extends Laptop {
+    private String operationSystem;
+
+    public Smartphone(String brand, String cpu, int ram, double price, long serialNumber, double displaySize, int batteryCapacity, String operationSystem) {
+        super(brand, cpu, ram, price, serialNumber, displaySize, batteryCapacity);
+        this.operationSystem = operationSystem;
+    }
+
+    @Override
+    public String toString() {
+        // call the super class's (parent class is Laptop) toString method
+        StringBuilder sb = new StringBuilder(super.toString());
+        // then we add (synonym "append") 2 Laptop-specific fields, please see above to understand why
+        sb.append(", operation system: ").append(operationSystem);
+        return sb.toString();
+
+    }
+
+}
