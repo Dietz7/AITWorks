87346987cf2dde271f98aed5860bcb0a1194ff82
Your Name <you@example.com>
2024-06-10 18:00:20 +0200

    lesson_32/code/homework_32/dao/StockImpl.java

--- lesson_32/code/homework_32/dao/StockImpl.java
+++ lesson_32/code/homework_32/dao/StockImpl.java
@@ -36,21 +36,39 @@ public class StockImpl implements Stock {
 
     @Override
     public Computer findWithDiscount(long serialNumber) {
-        return null;
+      for (int i = 0; i < size; i++) {
+        if(computers[i].getSerialNumber() == serialNumber && computers[i].hasDiscount()){
+              return computers[i];
+        }
+      }  
+      return null;
     }
 
     @Override
     public Computer removeComputer(long serialNumber) {
+      for (int i = 0; i < size; i++) {
+        if(computers[i].getSerialNumber() == serialNumber){
+          Computer removedComputer = computers[i];
+          for (int j = i; j < size - 1; j++) {
+            computers[j] = computers[j + 1];
+          }
+          computers[size - 1] = null;
+          size--;
+          return removedComputer;
+        }
+      }
         return null;
     }
 
     @Override
     public int quantity() {
-        return 0;
+        return size;
     }
 
     @Override
     public void printComputer() {
-
+      for (int i = 0; i < size; i++) {
+        System.out.println(computers[i]);
+      }
     }
 }
