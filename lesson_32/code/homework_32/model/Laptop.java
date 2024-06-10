67877a45c4ff17086f19d0b9de1fd03f0686e4df
Your Name <you@example.com>
2024-06-10 17:44:44 +0200

    lesson_32/code/homework_32/model/Laptop.java

--- lesson_32/code/homework_32/model/Laptop.java
+++ lesson_32/code/homework_32/model/Laptop.java
@@ -6,23 +6,17 @@ public class Laptop extends Computer {
     private double displaySize;
     private int batteryCapacity;
 
-    public Laptop(String brand, String cpu, int ram, double price, long serialNumber, double displaySize, int batteryCapacity) {
-        super(brand, cpu, ram, price, serialNumber);
+    public Laptop(String brand, String cpu, int ram, double price, boolean discount, long serialNumber, double displaySize, int batteryCapacity) {
+        super(brand, cpu, ram, price, discount, serialNumber);
         this.displaySize = displaySize;
         this.batteryCapacity = batteryCapacity;
     }
-
-
     @Override
     public String toString() {
-        // call the super class's (parent class is Computer) toString method
         StringBuilder sb = new StringBuilder(super.toString());
-        // then we add (synonym for word "append" - "add") 2 LAPTOP-specific fields, please see above to understand why
         sb.append(", display size: ").append(displaySize);
         sb.append(", battery capacity: ").append(batteryCapacity);
         return sb.toString();
-
     }
-
 }
 
