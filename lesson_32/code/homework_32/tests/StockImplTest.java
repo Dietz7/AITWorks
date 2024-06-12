89849e9998ac06f193c52d83a8bcd16737d25388
Your Name <you@example.com>
2024-06-12 23:46:30 +0200

    lesson_32/code/homework_32/tests/StockImplTest.java

--- lesson_32/code/homework_32/tests/StockImplTest.java
+++ lesson_32/code/homework_32/tests/StockImplTest.java
@@ -8,6 +8,8 @@ import homework_32.model.Smartphone;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static classwork.ArrayAppl.printArray;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,13 +22,15 @@ class StockImplTest {
         stock = new StockImpl(5);
         computers = new Computer[4];
 
-        computers[0] = new Computer("Asus", "i5", 256, 1200, true, 1234567);
-        computers[1] = new Computer("HP", "i6", 512, 1600, false, 7654321);
-        computers[2] = new Laptop("Lenovo", "i7", 128, 1800, true, 4444444,
-                15, 6);
+        computers[0] = new Laptop("Lenovo", "i7", 128, 1200, true, 1111111,
+                10.0, 12);
+        computers[1] = new Laptop("HP", "i8", 256, 1600, false, 2222222,
+                0.0, 10);
+        computers[2] = new Laptop("Sony", "i7", 512, 1800, true, 3333333,
+                15.0, 10);
         computers[3] = new Smartphone("Iphone", "i6", 6, 1000, true, 7777777,
-                5.6, 10, "iOS");
-        // it is necessary to add elements of array into object STOCK
+                10.0, 12, "iOS");
+
         for (Computer computer : computers) {
             stock.addComputer(computer);
         }
@@ -35,61 +39,72 @@ class StockImplTest {
 
     @Test
     void addComputer() {
-        // no null adding
-        assertFalse(stock.addComputer(null));
-        // no adding the identical computer second time
-        assertFalse(stock.addComputer(computers[1]));
-        Computer computer1 = new Computer("Acer", "i5", 256, 1400, false, 1234568);
-        // we have created and added a new computer
-        assertTrue(stock.addComputer(computer1));
-        assertEquals(5, stock.quantity()); // there are 5 articles in our stock
-        stock.printComputer();
-        Computer computer2 = new Computer("HP", "i6", 512, 1350, false, 7654323);
-        // we have created one more computer and exceeded our capacity, that is why we will be setting up a test,
-        // that shows it
-        assertFalse(stock.addComputer(computer2));
+        // now we are adding a new computer
+        Computer newComp = new Laptop("Asus", "i9", 256, 1400, false, 4444444,
+                0.0, 8);
+        assertTrue(stock.addComputer(newComp));
+        // adding a computer that exists - no adding must be done
+        assertFalse(stock.addComputer(computers[0]));
+        // adding one more computer so that we exceed our capacity
+        Computer extraComp = new Laptop("Lenovo", "i5", 128, 800, false, 5555555,
+                0.0, 8);
+        stock.addComputer(extraComp);
+        assertFalse(stock.addComputer(extraComp));
+
     }
 
     @Test
     void findComputer() {
-        assertEquals(computers[2], stock.findComputer(4444444)); // search of the computer by serial number
-        assertNull(stock.findComputer(1215146)); // search of the nonexistent computer
+        assertEquals(computers[2], stock.findComputer(3333333)); // search of the computer by serial number
+        assertNull(stock.findComputer(0000000)); // search of the nonexistent computer
+
     }
 
     @Test
     void findWithDiscount() {
-      assertEquals(computers[0], stock.findWithDiscount(1234567));
-      assertNull(stock.findWithDiscount(7654321)); // the defined computer has no discount
-      assertEquals(computers[2], stock.findWithDiscount(4444444));
-      assertEquals(computers[3], stock.findWithDiscount(7777777));
+        // Verify the first computer with a discount is returned
+        Computer discountComp = stock.findComputerWithDiscount();
+        assertNotNull(discountComp); // Ensure that a computer with a discount is found
+        assertTrue(discountComp.calcDiscount() < discountComp.getPrice());// Check if the discount
+        // amount is less than the original price
+
+        // Set discounts on multiple computers
+        if (computers[3] instanceof Smartphone) {
+            ((Smartphone) computers[3]).setDiscountPercentage(15.0);
+            //// Set a discount for testing on the fourth computer (Smartphone)
+        }
+        // Verify computers with discounts
+        discountComp = stock.findComputerWithDiscount();
+        assertNotNull(discountComp);
+        assertTrue(discountComp.calcDiscount() < discountComp.getPrice());
 
     }
 
     @Test
     void removeComputer() {
-        // now we are removing one computer
-        assertEquals(computers[0], stock.removeComputer(1234567));
+    // now we are removing one of the computers
+        assertEquals(computers[0], stock.removeComputer(1111111));
+        // check using quanity method if the computer was removed - by quantity(in setUp method 4 articles - 1 =3)
         assertEquals(3, stock.quantity());
         // check if we can remove the computer twice
-        assertNull(stock.removeComputer(1234567));
+        assertNull(stock.removeComputer(1111111));
         // test for checking if the removed computer can be found
-        assertNull(stock.removeComputer(1234567));
+        assertNull(stock.removeComputer(1111111));
         System.out.println("===========Demonstration of our actions from this test========");
         printComputer(); // demonstration of our actions from this test
-
     }
 
     @Test
     void quantity() {
         assertEquals(4, stock.quantity()); // intially  4 computers (in SetUpMethod)
-        stock.removeComputer(1234567);
+        stock.removeComputer(7777777);
         assertEquals(3, stock.quantity());
-        stock.printComputer(); // demonstration of our actions from this test
-
+        stock.printComputer();
     }
 
     @Test
     void printComputer() {
         stock.printComputer();
     }
+
 }
