01c1113adbed0b666028df96d4eeee76625d2c42
Your Name <you@example.com>
2024-06-12 23:45:02 +0200

    lesson_32/code/homework_32/model/Computer.java

--- lesson_32/code/homework_32/model/Computer.java
+++ lesson_32/code/homework_32/model/Computer.java
@@ -1,20 +1,43 @@
 package homework_32.model;
 
-public class Computer {
+public abstract class Computer {
+    public static final int BARCODE_LENGTH = 7;
     private String brand;
     private String cpu;
     private int ram;
     private double price;
+    private double discountPercentage;
     private boolean discount;
-    private long serialNumber;
 
-    public Computer(String brand, String cpu, int ram, double price, boolean discount, long serialNumber) {
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    private long msn; // manufacture serial number
+
+    public Computer(String brand, String cpu, int ram, double price, boolean discount, long msn) {
         this.brand = brand;
         this.cpu = cpu;
         this.ram = ram;
         this.price = price;
         this.discount = discount;
-        this.serialNumber = serialNumber;
+        this.msn = checkMsn(msn);
+    }
+
+    private long checkMsn(long msn) {
+        if (countDigit(msn) == BARCODE_LENGTH) {
+            return msn;
+        }
+        return -1;
+    }
+    private int countDigit(long msn) {
+        int count = 0;
+        do {
+            count++;
+            msn = msn / 10;
+        } while (msn != 0);
+
+        return count;
     }
 
     public String getBrand() {
@@ -49,21 +72,28 @@ public class Computer {
         this.price = price;
     }
 
-    public boolean hasDiscount(){
+    public boolean isDiscount() {
         return discount;
     }
-
-    public long getSerialNumber() {
-        return serialNumber;
+    public double getDiscountPercentage() {
+        return discountPercentage;
     }
 
-    public void setSerialNumber(long serialNumber) {
-        this.serialNumber = serialNumber;
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+    public long getMsn() {
+        return msn;
     }
 
-    private double displaySize;
-    private int batteryCapacity;
-    private String operationSystem;
+
+    // ABSTRACT Method to calculate the discount - that is why NO BODY  in this method
+    public abstract double calcDiscount();
+
+
+    //private double displaySize;
+    //private int batteryCapacity;
+    //private String operationSystem;
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Computer");
@@ -72,7 +102,7 @@ public class Computer {
         sb.append(", ram:  ").append(ram);
         sb.append(", price:  ").append(price);
         sb.append(", discount:  ").append(discount);
-        sb.append(", serial number: ").append(serialNumber);
+        sb.append(", msn: ").append(msn);
         return sb.toString();
     }
 }
