204763a6446ba7dc51221b2f78e9ae762816b229
Your Name <you@example.com>
2024-06-12 23:45:37 +0200

    lesson_32/code/homework_32/model/Laptop.java

--- lesson_32/code/homework_32/model/Laptop.java
+++ lesson_32/code/homework_32/model/Laptop.java
@@ -1,20 +1,33 @@
 package homework_32.model;
 
-import java.math.BigInteger;
-
 public class Laptop extends Computer {
-    private double displaySize;
+    private double discountPercentage;
     private int batteryCapacity;
 
-    public Laptop(String brand, String cpu, int ram, double price, boolean discount, long serialNumber, double displaySize, int batteryCapacity) {
-        super(brand, cpu, ram, price, discount, serialNumber);
-        this.displaySize = displaySize;
+    public Laptop(String brand, String cpu, int ram, double price, boolean discount, long msn, double discountPercentage, int batteryCapacity) {
+        super(brand, cpu, ram, price, discount, msn);
+        this.discountPercentage = discountPercentage;
         this.batteryCapacity = batteryCapacity;
     }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+
+    }
+    @Override
+    public double calcDiscount() {
+        if (isDiscount()) {  // Assuming isDiscount() is a getter for the discount field
+            return getPrice() - (getPrice() * discountPercentage / 100);
+        } else {
+            return getPrice();
+        }
+    }
+
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(super.toString());
-        sb.append(", display size: ").append(displaySize);
+        sb.append(", discount percentage: ").append(discountPercentage);
         sb.append(", battery capacity: ").append(batteryCapacity);
         return sb.toString();
     }
