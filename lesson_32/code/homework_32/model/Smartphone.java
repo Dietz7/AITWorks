8683f8abbc76f363c82625554e692d40e74087b1
Your Name <you@example.com>
2024-06-12 23:46:01 +0200

    lesson_32/code/homework_32/model/Smartphone.java

--- lesson_32/code/homework_32/model/Smartphone.java
+++ lesson_32/code/homework_32/model/Smartphone.java
@@ -3,9 +3,18 @@ package homework_32.model;
 public class Smartphone extends Laptop {
     private String operationSystem;
 
-    public Smartphone(String brand, String cpu, int ram, double price, boolean discount, long serialNumber, double displaySize, int batteryCapacity, String operationSystem) {
-        super(brand, cpu, ram, price, discount, serialNumber, displaySize, batteryCapacity);
+    public Smartphone(String brand, String cpu, int ram, double price, boolean discount, long msn, double discountPercentage, int batteryCapacity, String operationSystem) {
+        super(brand, cpu, ram, price, discount, msn, discountPercentage, batteryCapacity);
         this.operationSystem = operationSystem;
+    }
+
+    @Override
+    public double calcDiscount() {
+        if (isDiscount()) {
+            return getPrice() - (getPrice() * getDiscountPercentage() / 100);
+        } else {
+            return getPrice();
+        }
     }
 
     @Override
