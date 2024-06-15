870cb2e3e45c9f049226ee607e00fbf5af6c0006
Your Name <you@example.com>
2024-06-15 19:45:39 +0200

    lesson_37/code/homework_37/archive/dao/Archive.java

--- lesson_37/code/homework_37/archive/dao/Archive.java
+++ lesson_37/code/homework_37/archive/dao/Archive.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 public interface Archive {
     boolean addDocument(Document document); //The method takes a single parameter of TYPE DOCUMENT.
 // This is the object that is to be added.
-    boolean updateDocument(int documentId, int folderId);
+    boolean updateDocument(int documentId, int folderId, String title, String url);
     Document getDocumentFromArchive(int documentId, int folderId);
     Document[] getAllDocumentsFromFolder (int folderId);
     Document[] getDocumentBetweenDates(LocalDate dateFrom, LocalDate dateTo);
