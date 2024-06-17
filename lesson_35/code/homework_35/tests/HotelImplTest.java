d8a801c0e868585841b4e0f4a161e86a9ba1b621
Your Name <you@example.com>
2024-06-17 22:28:07 +0200

    lesson_35/code/homework_35/tests/

---
+++ lesson_35/code/homework_35/tests/HotelImplTest.java
@@ -0,0 +1,42 @@
+package homework_35.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelImplTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addPet() {
+    }
+
+    @Test
+    void removePet() {
+    }
+
+    @Test
+    void findPet() {
+    }
+
+    @Test
+    void findPetByBreed() {
+    }
+
+    @Test
+    void calculateTotalRevenue() {
+    }
+
+    @Test
+    void quantity() {
+    }
+
+    @Test
+    void printPet() {
+    }
+}
