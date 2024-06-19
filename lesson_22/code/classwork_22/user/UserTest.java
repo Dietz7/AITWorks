8a467cbc6a4ed9a79e46a09200ccc561101787e3
Your Name <you@example.com>
2024-06-19 22:05:38 +0200

    lesson_22/code/classwork_22/user/UserTest.java

--- lesson_22/code/classwork_22/user/UserTest.java
+++ lesson_22/code/classwork_22/user/UserTest.java
@@ -1,7 +1,6 @@
-package classwork.user;
+package classwork_22.user;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 
 public class UserTest {
