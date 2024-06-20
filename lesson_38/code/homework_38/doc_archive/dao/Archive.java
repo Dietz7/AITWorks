9334efe72790f21e01c152ee5083446f811992d7
Your Name <you@example.com>
2024-06-20 21:58:41 +0200

    lesson_38/code/homework_38/doc_archive/dao/Archive.java

--- lesson_38/code/homework_38/doc_archive/dao/Archive.java
+++ lesson_38/code/homework_38/doc_archive/dao/Archive.java
@@ -1,5 +1,18 @@
 package homework_38.doc_archive.dao;
 
+import homework_38.doc_archive.model.Document;
+
+import java.time.LocalDate;
+
 public interface Archive {
+boolean addDocument(Document document);
+boolean removeDocument(int folderId, int documentId);
+boolean updateDocument(int folderId, int documentId, String url);
+Document getDocFromFolder(int folderId, int DocumentId);
+Document [] getAllDocFromFolder(int folderId);
+Document [] getDocBetweenDate(LocalDate dateFrom, LocalDate dateTo);
+Document [] getDocBetweenDateTwo(LocalDate dateFrom, LocalDate dateTo);
+int size();
+void viewArchive();
 
 }
