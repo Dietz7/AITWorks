830dbe0ad795e6fe989915e71f1d9b0498245a25
Your Name <you@example.com>
2024-06-22 20:24:59 +0200

    lesson_39/code/homework_39/maraphon_task/tests/

---
+++ lesson_39/code/homework_39/maraphon_task/tests/MarketImplTest.java
@@ -0,0 +1,32 @@
+package homework_39.maraphon_task.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarketImplTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void removeProduct() {
+    }
+
+    @Test
+    void findProduct() {
+    }
+
+    @Test
+    void findProductByExpDate() {
+    }
+
+    @Test
+    void quantity() {
+    }
+
+    @Test
+    void printProduct() {
+    }
+}
