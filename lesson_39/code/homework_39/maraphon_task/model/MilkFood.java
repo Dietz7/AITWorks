6eeede42b731545cf7da6c0484d4f034335ee425
Your Name <you@example.com>
2024-06-24 21:13:05 +0200

    lesson_39/code/homework_39/maraphon_task/model/MilkFood.java

--- lesson_39/code/homework_39/maraphon_task/model/MilkFood.java
+++ lesson_39/code/homework_39/maraphon_task/model/MilkFood.java
@@ -5,8 +5,8 @@ public class MilkFood extends Product{
     private String milkType;
     private double fat;
 
-    public MilkFood(double price, String name, long barCode, String milkType, double fat) {
-        super(price, name, barCode);
+    public MilkFood(int id, double price, String name, String expDate, long barCode, String milkType, double fat) {
+        super(id, price, name, expDate,  barCode);
         this.milkType = milkType;
         this.fat = fat;
     }
@@ -27,11 +27,22 @@ public class MilkFood extends Product{
         this.fat = fat;
     }
 
+    public double getTotalPrice(Product[] products) {
+        double res = 0;
+        for (int i = 0; i < products.length; i++) {
+            res += products[i].getPrice();
+        }
+        return res;
+    }
+
+    private Product[] products;
+    private int size;
     @Override
-    public String toString() {
-        return "MilkFood{" +
-                "milkType='" + milkType + '\'' +
-                ", fat=" + fat +
-                '}';
+    public double getTotalCost() {
+        double totalCost = 0;
+        for (int i = 0; i < size; i++) {
+            totalCost += products[i].getPrice();
+        }
+        return totalCost;
     }
 }
