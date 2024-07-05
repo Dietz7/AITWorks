ad0bb8e252203657ee6ac62d8a53af9abdc498b3
Your Name <you@example.com>
2024-07-05 22:14:21 +0200

    lesson_45/code/classwork_45/primitive_stream/PrimitiveStreamExample.java

--- lesson_45/code/classwork_45/primitive_stream/PrimitiveStreamExample.java
+++ lesson_45/code/classwork_45/primitive_stream/PrimitiveStreamExample.java
@@ -1,5 +1,6 @@
 package classwork_45.primitive_stream;
 
+import java.util.IntSummaryStatistics;
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -14,6 +15,44 @@ public class PrimitiveStreamExample {
       //poluchit sluchajnuje chisla v vide potoka
         Stream<Integer> numbers = getRandomNumbers(N_NUMBERS, MIN, MAX);
         numbers.forEach(System.out::println);
+
+        System.out.println("-------------------Sum of Numbers---------------------------");
+
+        int sum = getRandomNumbers(N_NUMBERS, MIN, MAX)     // flow of objects
+                .mapToInt(Integer::intValue)
+                //.peek(System.out::println); // current print
+                .sum();
+
+        System.out.println("Sum = " + sum);
+
+        System.out.println("----------------------Summary Statistics-----------------------------");
+        IntSummaryStatistics intStat = getRandomNumbers(N_NUMBERS, MIN, MAX)
+                .mapToInt(Integer::intValue)
+                .summaryStatistics();
+
+        System.out.println("Min = " + intStat.getMin());
+        System.out.println("Max = " + intStat.getMax());
+        System.out.println("Average value = " + intStat.getAverage());
+        System.out.println("Sum = " + intStat.getSum());
+
+
+        System.out.println("------------------- Lotto 6 from 49 --------------------------");
+        new Random()
+                .ints(1, 50)
+                .distinct()
+                .limit(6)
+                .forEach(System.out::println);
+
+        System.out.println("----------------------------Shuffle------------------------------------");
+
+        int [] arr = {10, 20, 30, 40, 50};
+
+        new Random()
+                .ints(0, arr.length)
+                .distinct()
+                .limit(arr.length)
+                .forEach(i -> System.out.println(arr[i]));// thanks to i iterrating trhough our array
+
     } // end of main
 
     private static Stream<Integer> getRandomNumbers(int nNumbers, int min, int max) {
