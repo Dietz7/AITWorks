62a0ea9ed97bdda6d98909f4e26fc06e701bbcaa
Your Name <you@example.com>
2024-07-08 16:50:52 +0200

    lesson_45/code/homework_45/Student.java

--- lesson_45/code/homework_45/Student.java
+++ lesson_45/code/homework_45/Student.java
@@ -2,19 +2,22 @@ package homework_45;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+import java.util.Objects;
 
 public class Student {
     private int id;
     private String firstName;
     private String lastName;
-    private LocalDate birthDay;
-    private int course;
+    private String birthDay;
+    private String course;
     private int numGroup;
     private String country;
     private String gender;
 
-    public Student(int id, String firstName, String lastName, LocalDate birthday, int course, int numGroup, String country,
-                   String gender) {
+    public Student(int id, String firstName, String lastName, String birthDay, String course, int numGroup, String country, String gender) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -29,35 +32,76 @@ public class Student {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public LocalDate getBirthday() {
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDay() {
         return birthDay;
     }
 
-    public int getCourse() {
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getCourse() {
         return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
     }
 
     public int getNumGroup() {
         return numGroup;
     }
 
+    public void setNumGroup(int numGroup) {
+        this.numGroup = numGroup;
+    }
+
     public String getCountry() {
         return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public String getGender() {
         return gender;
     }
-    public int getAge() {
-        return Period.between(birthDay, LocalDate.now()).getYears();
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student student)) return false;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
@@ -66,11 +110,35 @@ public class Student {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", birthDay=" + birthDay +
-                ", course=" + course +
+                ", birthDay='" + birthDay + '\'' +
+                ", course='" + course + '\'' +
                 ", numGroup=" + numGroup +
                 ", country='" + country + '\'' +
                 ", gender='" + gender + '\'' +
                 '}';
+    }
+
+    //Comparator for Sorting by Birthday
+    public static Comparator<Student> birthdComparator = (s1, s2) -> {
+        //creation of DateTimeFormatter OBJECT that can parse dates in the format dd.MM.yyyy (day, month, year).
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        // now we parse the birtDay (which should be a String) of the 1st and 2nd Student (s1, s2) into a LocalDate object
+        // using the formatter.
+        LocalDate birthD1 = LocalDate.parse(s1.getBirthDay(), formatter);
+        LocalDate birthD2 = LocalDate.parse(s2.getBirthDay(), formatter);
+        return birthD1.compareTo(birthD2);
+    };
+
+    //now we want to calculate the age of the Student in years
+    public double getAge() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate birthD = LocalDate.parse(birthDay, formatter);
+        //This line gets the current date as a LocalDate object
+        // Эта строка получает текущую дату в виде объекта LocalDate
+        LocalDate currentDate = LocalDate.now();
+        //Calculation the number of years between birthDate and currentDate
+        long years = ChronoUnit.YEARS.between(birthD, currentDate);
+        //This line returns the calculated age as an integer
+        return(int) years;
     }
 }
