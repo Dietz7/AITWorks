33109e70d032be6027c43ffdf06ab003a21177a8
Your Name <you@example.com>
2024-07-08 19:12:11 +0200

    lesson_45/code/homework_45/DekanatAppl.java

--- lesson_45/code/homework_45/DekanatAppl.java
+++ lesson_45/code/homework_45/DekanatAppl.java
@@ -1,8 +1,10 @@
 package homework_45;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DekanatAppl {
